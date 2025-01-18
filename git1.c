013fe704a85ecd75bc2232ce514249ca62b76df8
Fayal Subedi <fayalsubedi4@gmail.com>
2025-01-18 17:23:39 +0530

    program on vowel or consonant

--- git1.c
+++ git1.c
@@ -1,5 +1,15 @@
+// program to find if a character is vowel or consonant
 #include<stdio.h>
+#include<ctype.h>
 int main(){
-    printf("hi everyone");
+    char x,y;
+    printf("enter a character");
+    scanf(" %c",&y);
+    x= tolower(y);
+    if ( x== 'a' || x=='e' || x== 'i' || x=='o' || x=='u')
+    printf("%c is vowel", x);
+    else {
+        printf("%c is conconant",x);
+    }
     return 0;
 }
